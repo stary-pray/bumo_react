af7a4d6891ae1ba5d077773f23164ff243508986
Dantong Huang <akistar518@gmail.com>
2016-03-07 22:55:31 +1300

    TagDetail react

--- src/containers/TagDetail/TagDetail.js
+++ src/containers/TagDetail/TagDetail.js
@@ -3,10 +3,14 @@ import {connect} from 'react-redux';
 import {bindActionCreators} from 'redux';
 import {loadTagDetail} from 'redux/modules/models/TagDetail';
 import {Link} from 'react-router';
+import PaintingInfo from 'components/PaintingInfo/PaintingInfo';
 
 
 @connect(
   (state, ownProps) => ({
+    painting: state.models.painting,
+    profile: state.models.profile,
+    paintingHeat: state.models.paintingHeat,
     tagDetail: state.models.tagDetail,
     name: ownProps.params.tagName,
     component: state.containers.TagDetail
@@ -22,20 +26,31 @@ export default class TagDetail extends Component {
     name: PropTypes.string,
     tagDetail: PropTypes.object,
     loadTagDetail: PropTypes.func,
-    component: PropTypes.object
+    component: PropTypes.object,
+    painting: PropTypes.object,
+    profile: PropTypes.object,
+    paintingHeat: PropTypes.object,
   };
 
 
-
-  componentWillMount(){
+  componentWillMount() {
     this.props.loadTagDetail(this.props.name);
     console.log(this.props);
   }
 
   render() {
     const {loaded, tagId} = this.props.component;
-    const {tagDetail} = this.props;
-    return ( <div>{loaded ? ( <h1>{tagDetail[tagId].name}</h1>
-    ):''}</div>);
+    const {tagDetail, painting,paintingHeat} = this.props;
+    return ( <div>
+      {loaded ?
+        <div>
+          <h1>{tagDetail[tagId].name}</h1>
+          <div>
+            {tagDetail[tagId].paintings? tagDetail[tagId].paintings.map((paintingId) => (
+              <PaintingInfo key={'painting' + paintingId} heat={paintingHeat[painting[paintingId].heat]}
+                            painting={painting[paintingId]}/>)):''}
+          </div>
+        </div> : ''}
+    </div>);
   }
 }
