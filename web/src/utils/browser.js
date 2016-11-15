ba9c9e3ec6ec5254880d825ac498ed3ad619815a
akinoniku <akinoniku@gmail.com>
2016-11-15 15:04:43 +0800

    browser block (fix)

---
+++ web/src/utils/browser.js
@@ -0,0 +1,18 @@
+import bowser from "bowser";
+
+const isUnsupported = bowser.isUnsupportedBrowser({
+  msie: "12",
+  edge: "13",
+  firefox: "40",
+  chrome: "43",
+  safari: "8",
+  opera: "35",
+  phantom: "534"
+}, window.navigator.userAgent);
+
+if(isUnsupported) {
+  alert('>A< 拜托别用古董浏览器！\n\n我们支持最新版的Chrome, Opera, Firefox, Safari, MS Edge，360浏览器，搜狗浏览器，QQ浏览器，百度浏览器等等等，但并不包括你现在正在使用的这个。\n\n=A= 要不我带你去下载一个Opera吧?');
+  window.document.location = 'http://www.opera.com/zh-cn';
+}
+
+
