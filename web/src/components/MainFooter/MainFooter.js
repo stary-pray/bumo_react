deff5ce489d06d9eebbc54af324f36a11b2b5bd4
akinoniku <akinoniku@gmail.com>
2016-11-18 17:54:28 +0800

    add qq group in footer

--- web/src/components/MainFooter/MainFooter.js
+++ web/src/components/MainFooter/MainFooter.js
@@ -10,8 +10,8 @@ export default class MainFooter extends Component {
         <span className="secondary-color"> © 2016 星祈 </span>
       </div>
       <div className="MainFooter__right">
-        <a className="secondary-color small-font hide" href="">关于星祈</a>
-        <span className="secondary-color small-font hide"> | </span>
+        <a className="secondary-color small-font" href="http://shang.qq.com/wpa/qunwpa?idkey=8e9e80abd52fa64506f08514fcfc17f7243e46195e60c79faee27c92074dbfba">恋绘QQ群: 539351114</a>
+        <span className="secondary-color small-font"> | </span>
         <a className="secondary-color small-font" href="http://www.miitbeian.gov.cn">粤ICP备14019972号</a>
       </div>
     </div>);
