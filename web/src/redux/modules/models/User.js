ad96907ed1bf309014d4a71207f8ea1082999505
akistar <akistar518@gmail.com>
2016-12-02 11:14:25 +1300

    User—Painter

--- web/src/redux/modules/models/User.js
+++ web/src/redux/modules/models/User.js
@@ -7,7 +7,7 @@ export const LOAD_USER_FAIL = 'bumo/painting/LOAD_USER_FAIL';
 export function loadUser(index) {
   return {
     types: [LOAD_USER, LOAD_USER_SUCCESS, LOAD_USER_FAIL],
-    promise: (client) => client.get('/api/profiles/hot?page=' + index),
+    promise: (client) => client.get('/api/profiles/painter?page=' + index),
     normalizeSchema: 'profile'
   };
 }
